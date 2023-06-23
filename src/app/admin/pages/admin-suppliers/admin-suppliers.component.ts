5c8a06b5679ab6d0aadef94005e358c403502d3c
Guilherme Pereira <guilhermepereira@defensoria.sc.gov.br>
2023-06-23 12:56:20 -0300

    Fix: appeard 0 or NAN on pagination total pages

--- src/app/admin/pages/admin-suppliers/admin-suppliers.component.ts
+++ src/app/admin/pages/admin-suppliers/admin-suppliers.component.ts
@@ -64,7 +64,8 @@ export class AdminSuppliersComponent implements OnInit {
       .subscribe((data) => {
         const sortedData = this.sortContentTableAlphabetically(data);
         this.response = new MatTableDataSource(sortedData.results);
-        this.response.count = data.next;
+        this.response.next = data?.next;
+        this.response.count = data?.count;
       });
   }
 
