64368753e47bf63217c61e227ea83f32f71ba42f
Vinicius Nau <viniciuscoelhonau975@gmail.com>
2023-09-06 15:56:04 -0300

    fix: Environment

--- src/app/environments/env.environment.ts
+++ src/app/environments/env.environment.ts
@@ -2,4 +2,4 @@ import { localEnvironment } from './local.environment';
 import { devEnvironment } from './dev.environment';
 import { prodEnvironment } from './prod.environment';
 
-export const environment = localEnvironment;
+export const environment = prodEnvironment;
